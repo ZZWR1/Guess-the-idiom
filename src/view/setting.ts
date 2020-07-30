f91b06058b1852738cf3e8e0e8e5ecf9239978ea
ZZWR1 <2198762063@qq.com>
2020-07-30 19:08:02 +0800

    sound

--- src/view/setting.ts
+++ src/view/setting.ts
@@ -1,11 +1,27 @@
-class setting extends eui.Component implements  eui.UIComponent {
+class setting extends eui.Component implements eui.UIComponent {
+	private btn_music: eui.CheckBox;
+	public static Setting: setting;
+	public static setting() {
+		if (setting.Setting == null) {
+			setting.Setting = new setting();
+			return setting.Setting;
+		}
+	}
 	public constructor() {
 		super();
+		this.skinName = "settingSkin";
+		this.btn_music.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onclick, this);
 	}
 
-	protected childrenCreated():void
-	{
-		super.childrenCreated();
+	private onclick(e: Event) {
+
+		let SM = SoundMenager.sound();
+		if (this.btn_music.selected == true) {
+			SM.start();
+		} else {
+			SM.stopsound();
+		}
+
 	}
-	
+
 }
